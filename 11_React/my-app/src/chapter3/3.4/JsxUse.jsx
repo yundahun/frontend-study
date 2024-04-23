00e948522ea9e667217285ace46d32471d028cdf
dahun <fkdlwldj02@naver.com>
2024-04-23 16:51:56 +0900

    2024.04.23 수업 업로드

---
+++ 11_React/my-app/src/chapter3/3.4/JsxUse.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+function JsxUse(params) { // 컴포넌트명, 함수명 동일하게
+  //JSX의 사용법(문법)
+  // 1. 꼭 닫혀야 하는 태그
+  // const element = (
+  //   <div>
+  //     {/* <div> */}
+  //     {/* <input type="text"> */}
+  //     {/* <br> */}
+
+  //     {/* HTML에서는 태그를 닫지 않고 사용가능
+  //       리액트에서는 태그를 열었으면 무조건 닫아야 함*/ }
+  //     <div></div>
+  //     <input type="text" />
+  //     <br />
+  //   </div>
+  // );
+
+  // 2. 꼭 감싸져야만 하는 태그
+  // 두개 이상의 태그는 무조건 하나의 태그롤 감싸져있어야 함
+  // const element = (
+  //   // 반드시 하나의 부모태그로 감싸져야 함
+  //   // <div>
+  //   // <div>안녕하세요.</div>
+  //   // <div>안녕히계세요.</div>
+  //   // </div>
+
+  //   // 이렇게 단순히 감싸기 위하여 불필요한 div로 감싸는게 별로 좋지 않을 수 있음
+  //   // HTML 중첩 구조가 복잡해지며 그에 따른 스타일 설정을 할 때도 복잡해짐
+  //   // 이때 사용하는 것이 React Fragment
+
+  //   // React.Fragment는 생략 가능
+  //   <>
+  //     <div>안녕하세요.</div>
+  //     <div>안녕히계세요.</div>
+  //   </>
+  // );
+
+  // 3. JSX 안에 자바스크립트 값 또는 표현식 사용하기
+  // 자바스크립트 변수(또는 함수)를 사용할 때는 {} 사용
+  const name = 'react';
+
+  const user = {
+    firstName: 'Inbong',
+    lastName: 'Kim'
+  };
+
+  const formatName = (user) => {
+    return `${user.firstName} ${user.lastName}`;
+  }
+  
+  const element = (
+    <>
+      <div>{name} 배우기</div>
+      <h1>Hello, {formatName(user)} </h1>
+    </>
+  );
+
+  return element;
+}
+
+export default JsxUse; // JsxUse 함수 컴포넌트 내보내기
+// export를 해줘야 다른곳에서 import 해서 사용 가능
---
+++ 11_React/my-app/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import JsxUse from './chapter3/3.4/JsxUse';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Root DOM Node에 리액트 컴포넌트를 렌더링 하도록 하는 함수(React 엘리먼트를 DOM 엘리먼트로 렌더링하는 역할)
+// 처음으로 렌더링할 컴포넌트를 지정하는데 App 컴포넌트가 기본적으로 들어가있음
+root.render(
+    // <App />
+
+    // 3장 예제
+    <JsxUse />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
