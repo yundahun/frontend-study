778984985b650a3e5158183ba9a98ff3af7678d4
dahun <fkdlwldj02@naver.com>
2024-04-25 16:55:26 +0900

    수정

--- 11_React/my-app/src/chapter5/Comment.jsx
+++ 11_React/my-app/src/chapter5/Comment.jsx
@@ -48,10 +48,10 @@ function Comment(props) {
 
       {/* 댓글 작성자와 내용 */}
       <div style={styles.contentContainer}>
-        <span style={styles.nameText}>{props.name}</span>
+        <span style={styles.nameText}>{props.user.name}</span>
 
         <span style={styles.contentText}>
-        {props.content}
+        {props.user.content}
         </span>
       </div>
     </div>
--- 11_React/my-app/src/chapter5/CommentList.jsx
+++ 11_React/my-app/src/chapter5/CommentList.jsx
@@ -5,9 +5,10 @@ import Comment from './Comment';
 function CommentList(props) {
   return (
     <div>
-      <Comment name="윤다훈" content="목표는 props 마스터"/>
+      {/* <Comment name="윤다훈" content="목표는 props 마스터"/>
       <Comment name="최현아" content="메랜 할 사람"/>
-      <Comment name="장원석" content="전 운동하겠습니다"/>
+      <Comment name="장원석" content="전 운동하겠습니다"/> */}
+      <Comment user = {{name:"장원석", content:"전 운동하겠습니다"}}/>
 
       {/* Quiz: props를 사용하여 name과 content 값 전달(데이터는 자유롭게 작성) */}
     </div>
