f7131abf7dbac1bc9351f0a40ca7e9d64134ae47
dahun <fkdlwldj02@naver.com>
2024-04-29 16:47:00 +0900

    2024.04.29 수업 업로드

---
+++ 11_React/my-app/src/chapter8/8.1/Toggle.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Toggle(props) {
+  const [isToggleOn,setIsToggleOn] = useState(true); // 배열을 하나 뱉어냄 [초기값, 상태변경함수] - 구조분해할당
+
+  // 방법 1. 함수 선언문 사용
+  // function handleToggle() {
+  //   setIsToggleOn(!isToggleOn);
+  // }
+  // 방법 2. 화살표 함수 사용
+  const handleToggle = () => {
+    setIsToggleOn(!isToggleOn);
+  }
+
+  return (
+    // 이벤트는 카멜 키이스(camelCase)를 사용
+    // 함수(이벤트 핸들러)를 바로 전달하면 됨
+    // 주의! handleToggle() 로 작성 시 함수가 바로 호출됨
+    <button type='button' onClick={handleToggle}>
+      {isToggleOn ? '켜짐' : '꺼짐'}
+    </button>
+  );
+}
+
+export default Toggle;
