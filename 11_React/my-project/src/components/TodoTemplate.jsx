3a7388014e9028a4835bc623e48a73b784cdd9f1
yundahun <fkdlwldj02@naver.com>
2024-05-21 21:56:13 +0900

    템플릿수정

--- 11_React/my-project/src/components/TodoTemplate.jsx
+++ 11_React/my-project/src/components/TodoTemplate.jsx
@@ -5,7 +5,15 @@ const TemplateBlock = styled.div`
   height: 768px;
 
   position: relative;
+  background: white;
+  border-radius: 16px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+  margin: 0 auto;
 
+  margin-top: 96px;
+  margin-bottom: 32px;
+  display: flex;
+  flex-direction: column;
 `
 
 
